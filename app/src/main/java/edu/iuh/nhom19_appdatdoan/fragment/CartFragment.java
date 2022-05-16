654a9c288511f29fe5d50ed677a4236d309077f6
nguyenTanHung11222 <67372290+nguyenTanHung11222@users.noreply.github.com>
2022-05-16 09:14:06 +0700

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 0bac77ddc114575d38f43c21ae091d8c8a8d0feb, reversing
    changes made to db6d5ed9348c72a22bb22e784ba5ef0c54c30438.

--- app/src/main/java/edu/iuh/nhom19_appdatdoan/fragment/CartFragment.java
+++ app/src/main/java/edu/iuh/nhom19_appdatdoan/fragment/CartFragment.java
@@ -7,35 +7,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.GridView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import edu.iuh.nhom19_appdatdoan.R;
-import edu.iuh.nhom19_appdatdoan.adapter.CakeAdapter;
-import edu.iuh.nhom19_appdatdoan.entity.Cake;
-import edu.iuh.nhom19_appdatdoan.entity.HoaDon;
 
 
-public class    CartFragment extends Fragment {
+public class CartFragment extends Fragment {
 
-    GridView glListcart;
-    CakeAdapter cakeCart;
-    List<Cake> cakeCarts;
-    int tongTien=0;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_cart, container, false);
-        glListcart = view.findViewById(R.id.gvListCart);
-  //      hoaDon = new HoaDon();
-  //      cakeCart = new CakeAdapter(getActivity(), R.layout.item_cart, cakeCarts);
-        glListcart.setAdapter(cakeCart);
-        for (Cake cake : cakeCarts) {
-            tongTien=tongTien+cake.getPriceCake();
-        }
-        return view;
+        return inflater.inflate(R.layout.fragment_cart, container, false);
     }
 }
